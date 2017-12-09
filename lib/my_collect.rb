6f99169316c83c9b631c2520ae726ab074b476bd
Christopher Diep <diep.christopher@gmail.com>
2017-12-09 10:14:36 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  counter = 0
+  new_collection = []
+  while counter < collection.size
+    yield collection.split(" ").first
+    new_collection << collection.split(" ").first
+    counter += 1
+  end
+end
