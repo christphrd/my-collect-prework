acad4263512800e31df1f50edd2e80f908a2263c
Christopher Diep <diep.christopher@gmail.com>
2017-12-09 10:24:33 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(collection)
   counter = 0
   new_collection = []
   while counter < collection.size
-    yield collection[counter].split(" ").upcase
-    new_collection << collection[counter].split(" ").upcase
+    yield collection[counter].split(" ")
+    new_collection << collection[counter].split(" ")
     counter += 1
   end
   new_collection
