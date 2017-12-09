3cff0c7f0a71e2574f5dfec877ee6e0e4b1dcc54
Christopher Diep <diep.christopher@gmail.com>
2017-12-09 10:26:47 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(collection)
   counter = 0
   new_collection = []
   while counter < collection.size
-    yield collection[counter].upcase
-    new_collection << collection[counter].upcase
+    yield collection[counter]
+    new_collection << collection[counter]
     counter += 1
   end
   new_collection
